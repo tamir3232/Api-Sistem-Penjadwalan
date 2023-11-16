3b5a5e44d6fc9faf58ae830fdab314eaafbd23d6
Tamir <tamirrasydi@gmail.com>
2023-11-16 14:04:34 +0700

    fix jam

--- app/Http/Controllers/Jam/JamController.php
+++ app/Http/Controllers/Jam/JamController.php
@@ -18,9 +18,9 @@ class JamController extends Controller
     {
         // if (Auth::user()->role == 1)
         // {
-            $jam =  DB::table('jam')->get();
-            return JamResources::collection($jam);
-            return $jam;
+        $jam =  DB::table('jam')->orderBy('awal')->get();
+        return JamResources::collection($jam);
+        return $jam;
         // }
         // return['Anda tidak memiliki akses'];
     }
@@ -29,22 +29,21 @@ class JamController extends Controller
 
     public function store(Request $request)
     {
-        if (Auth::user()->role == 1)
-        {
+        if (Auth::user()->role == 1) {
             $request->validate([
-                'range_jam'=>'required',
-                'awal'=>'required',
-                'akhir'=>'required'
+                'range_jam' => 'required',
+                'awal' => 'required',
+                'akhir' => 'required'
             ]);
 
             $add = Jam::create([
-            'range_jam'=>$request->range_jam,
-            'awal'=>$request->awal,
-            'akhir'=>$request->akhir,
+                'range_jam' => $request->range_jam,
+                'awal' => $request->awal,
+                'akhir' => $request->akhir,
             ]);
             return $add;
         }
-        return['Anda tidak memiliki akses'];
+        return ['Anda tidak memiliki akses'];
     }
 
     /**
@@ -52,16 +51,14 @@ class JamController extends Controller
      */
     public function show($id)
     {
-        if (Auth::user()->role == 1)
-        {
-            $jamexist=Jam::where('id','=',$id)->first();
-            if ($jamexist)
-                {
-                    return new JamResources($jamexist);
-                };
+        if (Auth::user()->role == 1) {
+            $jamexist = Jam::where('id', '=', $id)->first();
+            if ($jamexist) {
+                return new JamResources($jamexist);
+            };
             return ['jam tidak tersedia'];
         }
-        return['Anda tidak memiliki akses'];
+        return ['Anda tidak memiliki akses'];
     }
 
 
@@ -69,41 +66,38 @@ class JamController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request,$id)
-        {
-            if (Auth::user()->role == 1)
-            {
-                $jamexist = Jam::where('id','=',$id)->first();
-                if ($jamexist)
-                {$jamexist -> update(
-                [
-                'awal' => $request->awal ?? $jamexist ->awal,
-                'range_jam' => $request -> range_jam ?? $jamexist -> range_jam,
-                'akhir' => $request-> akhir ?? $jamexist ->akhir,
-                ]);
-                return['Update success'];
-                }
-                return['Jam tidak ditemukan'];
+    public function update(Request $request, $id)
+    {
+        if (Auth::user()->role == 1) {
+            $jamexist = Jam::where('id', '=', $id)->first();
+            if ($jamexist) {
+                $jamexist->update(
+                    [
+                        'awal' => $request->awal ?? $jamexist->awal,
+                        'range_jam' => $request->range_jam ?? $jamexist->range_jam,
+                        'akhir' => $request->akhir ?? $jamexist->akhir,
+                    ]
+                );
+                return ['Update success'];
             }
-            return['Anda tidak memiliki akses'];
+            return ['Jam tidak ditemukan'];
         }
+        return ['Anda tidak memiliki akses'];
+    }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
-    {   if (Auth::user()->role == 1)
-        {
-            $jamexist = Jam::where('id','=',$id)->first();
-            if ($jamexist)
-                {
-                $jamexist -> delete();
+    {
+        if (Auth::user()->role == 1) {
+            $jamexist = Jam::where('id', '=', $id)->first();
+            if ($jamexist) {
+                $jamexist->delete();
                 return ['Jam berhasil di hapus'];
-                }
-                return['jam tidak ditemukan'];
+            }
+            return ['jam tidak ditemukan'];
         }
-        return['Anda tidak memiliki akses'];
+        return ['Anda tidak memiliki akses'];
     }
-
-
 }
